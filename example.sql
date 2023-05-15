c2a47c365040fbd554a04eb40f98587e69f8be24
Fateme karami <99094357+Ermiti@users.noreply.github.com>
2023-05-15 08:15:47 +0330

    Create example.sql

---
+++ example.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db;
+CREATE TABLE myTable (
+    theName nvarchar(255) NOT NULL,
+    question nvarchar(2000) NOT NULL,
+    email varchar(255) NOT NULL,
+);
+
+
+
+
+INSERT INTO myTable (theName, question, email) VALUES (Fateme karami, ddddddddddd, k5084025@gmail.com);
